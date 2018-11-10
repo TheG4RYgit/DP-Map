329789fcf6e142dbe61584f5665fd499d2776653
Nathan Cramer <Spiritofpower24@gmail.com>
2018-11-10 14:58:43 -0600

    Added dummy frags and first pass on buttons

--- Application/app/src/main/java/dpmapteam/dpmap/MainActivity.java
+++ Application/app/src/main/java/dpmapteam/dpmap/MainActivity.java
@@ -3,9 +3,12 @@ package dpmapteam.dpmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+        ImageButton AddButton = (ImageButton) findViewById(R.id.addbut);
+        AddButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Fragment addfrag = new AddFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.add_fragment_frame, addfrag,
+                        addfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton OptionsButton = (ImageButton) findViewById(R.id.setbut);
+        OptionsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment optfrag = new OptionsFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.opt_fragment_frame, optfrag,
+                        optfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton RemoveButton = (ImageButton) findViewById(R.id.rmvbut);
+        RemoveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment rmvfrag = new RemoveFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.rmv_fragment_frame, rmvfrag,
+                        rmvfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton SidebarButton = (ImageButton) findViewById(R.id.hambut);
+        SidebarButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment sbrfrag = new SidebarFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.sbr_fragment_frame, sbrfrag,
+                        sbrfrag.getClass().getSimpleName()).addToBackStack(null).commit();
             }
         });
     }
