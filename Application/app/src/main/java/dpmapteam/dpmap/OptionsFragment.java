99848d485581d5f5f1b6b97668c34d1c7807a380
CSFaubion <cameronfaubion@gmail.com>
2018-11-14 23:33:39 -0600

    options frag attempt on click not working for radio buttons.

--- Application/app/src/main/java/dpmapteam/dpmap/OptionsFragment.java
+++ Application/app/src/main/java/dpmapteam/dpmap/OptionsFragment.java
@@ -1,7 +1,5 @@
 package dpmapteam.dpmap;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,28 +7,72 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class OptionsFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-   //     return super.onCreateView(inflater, container, savedInstanceState);
 
+    private Settings OptSettings;
 
-        View thisView = inflater.inflate(R.layout.fragment_options, container, false);
+    public void onRadioButtonClicked(View v)  {
+        //this will tell you when an option is selected.
+        boolean checked = ((RadioButton) v).isChecked();
 
-        CheckBox ifCheck = thisView.findViewById(R.id.HighPin1);
-
-        return thisView;
-
+        switch(v.getId())
+        {
+            case R.id.Highlight:
+                if(checked)
+                    OptSettings.IsPin = false;
+                break;
+            case R.id.Pin:
+                if(checked)
+                    OptSettings.IsPin = true;
+                break;
+            case R.id.MarkOne:
+                if(checked)
+                    OptSettings.DisplayAll = false;
+                break;
+            case R.id.MarkAll:
+                if(checked)
+                    OptSettings.DisplayAll = true;
+                break;
+        }
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        OptSettings = Settings.getInstance();
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View parentView = inflater.inflate(R.layout.fragment_options, container, false);
 
+        RadioGroup HighPin = parentView.findViewById(R.id.radioHiPinGroup);
+        RadioGroup MarkStyle = parentView.findViewById(R.id.radioMarkGroup);
+
+        RadioButton pinopt = parentView.findViewById(R.id.Pin);
+        pinopt.setOnClickListener();
+
+        if (OptSettings.IsPin)
+            HighPin.check(R.id.Pin);
+        else
+            HighPin.check(R.id.Highlight);
+
+        if (OptSettings.DisplayAll)
+            MarkStyle.check(R.id.MarkOne);
+        else
+            MarkStyle.check(R.id.MarkOne);
+
+        return parentView;
+    }
 
 }
 
-//get function for OnCreateView
+
+
+
--- Application/app/src/main/java/dpmapteam/dpmap/Settings.java
+++ Application/app/src/main/java/dpmapteam/dpmap/Settings.java
@@ -9,9 +9,6 @@ public class Settings {
     private static Settings SettingsInstance = null;
     public boolean IsPin;
     public boolean DisplayAll;
-    ArrayList<String> OptionList = new ArrayList<String>();
-
-
 
     private Settings()
     {
@@ -27,54 +24,4 @@ public class Settings {
         }
         return SettingsInstance;
     }
-
-   /**
-    public void selectOption(view View)
-    {
-        boolean checked = ((CheckBox) android.view.View).isChecked();
-            switch (android.view.View.getId())
-            {
-                case : R.id.HighPin1:
-                {
-
-
-                    OptionList.add("Highlight");
-                    if(checked)
-                    {
-                        OptionList.remove("Pin")
-                    }
-                }
-                case : R.id.HighPin2:
-                {
-
-
-                    OptionList.add("Pin");
-                    if(checked)
-                    {
-                        OptionList.remove("Highlight")
-                    }
-                }
-                case : R.id.HighPin3:
-                {
-
-
-                    OptionList.add("All at once");
-                    if(checked)
-                    {
-                        OptionList.remove("One at a time")
-                    }
-                }
-                case : R.id.HighPin4:
-                {
-
-
-                    OptionList.add("One at a time");
-                    if(checked)
-                    {
-                        OptionList.remove("All at once")
-                    }
-                }
-            }
-    }
-    */
 }
