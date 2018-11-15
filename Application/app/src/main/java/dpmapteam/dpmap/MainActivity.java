842cde2d5514ab5cea593126b5310b56ef5195a5
Nathan Cramer <Spiritofpower24@gmail.com>
2018-11-14 19:45:49 -0600

    Fix to onBackPressed
    Fixed onBackPressed to reset onClickListeners because apparently that's something you have to do

--- Application/app/src/main/java/dpmapteam/dpmap/MainActivity.java
+++ Application/app/src/main/java/dpmapteam/dpmap/MainActivity.java
@@ -119,6 +119,44 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onBackPressed();
         setContentView(R.layout.activity_main);
+
+        ImageButton AddButton = (ImageButton) findViewById(R.id.addbut);
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment addfrag = new AddFragment();
+                getSupportFragmentManager().popBackStack();
+                getSupportFragmentManager().beginTransaction().replace(R.id.add_fragment_frame, addfrag,
+                        addfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton OptionsButton = (ImageButton) findViewById(R.id.setbut);
+        OptionsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment optfrag = new OptionsFragment();
+                getSupportFragmentManager().popBackStack();
+                getSupportFragmentManager().beginTransaction().replace(R.id.opt_fragment_frame, optfrag,
+                        optfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton RemoveButton = (ImageButton) findViewById(R.id.rmvbut);
+        RemoveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment rmvfrag = new RemoveFragment();
+                getSupportFragmentManager().popBackStack();
+                getSupportFragmentManager().beginTransaction().replace(R.id.rmv_fragment_frame, rmvfrag,
+                        rmvfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+        ImageButton SidebarButton = (ImageButton) findViewById(R.id.hambut);
+        SidebarButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment sbrfrag = new SidebarFragment();
+                getSupportFragmentManager().popBackStack();
+                getSupportFragmentManager().beginTransaction().replace(R.id.sbr_fragment_frame, sbrfrag,
+                        sbrfrag.getClass().getSimpleName()).addToBackStack(null).commit();
+            }
+        });
+
         ClassList CList = ClassList.getInstance();
         Settings opt = Settings.getInstance();
         CList.selectedClass = 0;
